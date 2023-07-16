33e4d2978548d4282e21720a31da34288de3c370
Starlord2000-cpu <75923133+Starlord2000-cpu@users.noreply.github.com>
2023-07-17 01:12:10 +0530

    final release version 1.0.0

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -1,55 +1,71 @@
 import { useEffect } from "react";
-import { useDispatch,useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setPosts } from "state";
 import PostWidget from "./PostWidget";
+import { Typography } from "@mui/material";
 
-const PostsWidget=({userId, isProfile=false})=>{
-
-     const dispatch = useDispatch();
-     const posts = useSelector((state)=>state.posts);
-     const token   = useSelector((state)=>state.token);
-     
-    const getPosts = async () =>{
-    const response = await  fetch("http://localhost:3001/posts",{
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
+  const getPosts = async () => {
+    try{
+      const response = await fetch("http://localhost:3001/posts", {
         method: "GET",
-        headers: {Authorization: `Bearer ${token}`},
-    });
-    const data = await response.json();
-    dispatch(setPosts({posts:data}));
-     }
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      const data = await response.json();
+      console.log(data);
+      dispatch(setPosts({ posts: data }));
+    } catch(err){
+      console.log(err);
+    }
+  };
+  
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+      );
+      const data = await response.json();
+      dispatch(setPosts({ posts: data }));
+    };
+    
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+  
+   
 
-     const getUserPosts = async () =>{
-        const response = await  fetch(`http://localhost:3001/posts/${userId}/posts`,{
-            method: "GET",
-            headers: {Authorization: `Bearer ${token}`},
-        });
-        const data = await response.json();
-        dispatch(setPosts({posts:data}));
-         };
-    useEffect(()=>{
-        if(isProfile){
-            getUserPosts();
-        }else{
-            getPosts();
-        }
-    },[]); 
 
-    return (
-        <>
-         {posts.map(
-         ({
-            _id,
-            userId,
-            firstName,
-            lastName,
-            description,
-            location,
-            picturePath,
-            userPicturePath,
-            likes,
-            comments,
-         })=>(
-            <PostWidget 
+
+
+const funct = (value)=>{
+
+}
+  return (
+    <>
+        {posts.map(
+        ({
+          _id,
+          userId,
+          firstName,
+          lastName,
+          description,
+          location,
+          picturePath,
+          userPicturePath,
+          likes,
+          comments,
+        }) => (
+          <PostWidget
             key={_id}
             postId={_id}
             postUserId={userId}
@@ -60,14 +76,11 @@ const PostsWidget=({userId, isProfile=false})=>{
             userPicturePath={userPicturePath}
             likes={likes}
             comments={comments}
-            />
-         )  
-
-         )}
-        
-        </>
-    )
-
+          />
+        )
+      )}
+    </>
+  );
 };
 
 export default PostsWidget;
