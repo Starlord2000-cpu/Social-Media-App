410a705ce434750a7925388906fc9090c100017c
Starlord2000-cpu <75923133+Starlord2000-cpu@users.noreply.github.com>
2023-07-17 01:49:04 +0530

    fin 1.0.0

--- Server/controllers/users.js
+++ Server/controllers/users.js
@@ -23,8 +23,8 @@ export const getUserFriends = async (req,res)=>{
              user.friends.map((id)=>User.findById(id))
          );
          const formattedFriends = friends.map(
-          ({_id,firstname,lastname,occupation,location,picturePath})=>{
-              return {_id,firstname,lastname,occupation,location,picturePath};
+          ({_id,firstName,lastName,occupation,location,picturePath})=>{
+              return {_id,firstName,lastName,occupation,location,picturePath};
           }
          );
          res.status(200).json(formattedFriends);
@@ -60,9 +60,9 @@ export const addRemoveFriend = async (req,res)=>{
         user.friends.map((id)=>User.findById(id))
     );
     const formattedFriends = friends.map(
-     ({_id,firstname,lastname,occupation,location,picturePath})=>{
+     ({_id,firstName,lastName,occupation,location,picturePath})=>{
 
-         return {_id,firstname,lastname,occupation,location,picturePath};
+         return {_id,firstName,lastName,occupation,location,picturePath};
      }
     );
   res.status(200).json(formattedFriends);
--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -15,7 +15,6 @@ const PostsWidget = ({ userId, isProfile = false }) => {
         headers: { Authorization: `Bearer ${token}` },
       });
       const data = await response.json();
-      console.log(data);
       dispatch(setPosts({ posts: data }));
     } catch(err){
       console.log(err);
@@ -47,9 +46,8 @@ const PostsWidget = ({ userId, isProfile = false }) => {
 
 
 
-const funct = (value)=>{
 
-}
+
   return (
     <>
         {posts.map(
