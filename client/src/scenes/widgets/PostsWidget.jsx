ea1adc2fd9aa16787da2b4df9fc440867050dc51
Starlord2000-cpu <75923133+Starlord2000-cpu@users.noreply.github.com>
2023-07-27 19:26:56 +0530

    deployed version

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -10,7 +10,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   const token = useSelector((state) => state.token);
   const getPosts = async () => {
     try{
-      const response = await fetch("http://localhost:3001/posts", {
+      const response = await fetch("https://sociopedia-0p0o.onrender.com/posts", {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
       });
@@ -23,7 +23,7 @@ const PostsWidget = ({ userId, isProfile = false }) => {
   
   const getUserPosts = async () => {
     const response = await fetch(
-      `http://localhost:3001/posts/${userId}/posts`,
+      `https://sociopedia-0p0o.onrender.com/posts/${userId}/posts`,
       {
         method: "GET",
         headers: { Authorization: `Bearer ${token}` },
